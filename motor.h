e87caf14bd0b99348b2fa40a7cfe51e2b4c8a3e2
tuqataqie <tuqataqiie415@gmail.com>
2026-05-14 23:14:30 +0300

    Add files via upload

---
+++ motor.h
@@ -0,0 +1,9 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+#include "mbed.h"
+
+void motor_init();
+void car_forward(float dc);
+void car_stop();
+
+#endif
---
+++ ultrasonic.h
@@ -0,0 +1,7 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+#include "mbed.h"
+
+float get_range();
+
+#endif
